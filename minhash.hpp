4d88ae884f3ce9c552dd72a916c90606c2a63a2e
TAYYU <sharisha@cs.stonybrook.edu>
2017-11-05 00:17:47 -0400

    shreyas - 3: down_sample function and some data members declaration

--- minhash.hpp
+++ minhash.hpp
@@ -11,17 +11,21 @@ class CountEstimator
     Still don't know what max_prime is */
 private:
 	int n;
-	long _max_prime = 9999999999971;
-	int _ksize = 0;
-	std::string _input_file_name = NULL;
-	char _save_kmers = 'n';
-	std::list<long> _hash_list ;
-	bool _rev_comp=false;
+	long max_prime = 9999999999971;
+	int ksize = 0;
+	std::string input_file_name = NULL;
+	char save_kmers = 'n';
+	std::list<long> hash_list ;
+	bool rev_comp=false;
+	std::list<long> _mins ;
+	std::list<long> _counts ;
+	std::list<std::string> _kmers ;
+	long _true_num_kmers;
 
 public:
 	CountEstimator(T& object, std::list<long> hash_list, int n = 0, long max_prime = 9999999999971, int ksize = 0, std::string input_file_name = NULL, char save_kmers = 'n', bool rev_comp = false);
-	void parse_file();
-	void down_sample();
+	void parse_file(bool rev_comp=false);
+	void down_sample(long h);
 	void add();
 	void add_sequence();
 	void jaccard_count();

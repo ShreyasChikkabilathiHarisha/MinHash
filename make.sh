545e83c57376093b60cb145512b2d507e07f6a9b
Gopalakrishnan <krishnangounder@bitbucket.org>
2017-11-04 23:52:21 -0400

    adding basic workflow file

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 rm ./vegeta.out
 echo "********** Compiling ***********"
 g++ minhash.cpp -o vegeta.out
+g++ --std=c++11 bloom_filter_for_CH.cpp -o bloom_filter_for_CH
 echo "********** executing ***********"
 ./vegeta.out

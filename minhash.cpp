a5dc661d43655815fbf1051f34f8856ff35fa618
TAYYU <sharisha@cs.stonybrook.edu>
2017-11-04 22:18:44 -0400

    shreyas - 2 : initialization of data members and few prime functions

--- minhash.cpp
+++ minhash.cpp
@@ -3,8 +3,31 @@
 using namespace std;
 
 template <typename T>
-CountEstimator<T>::CountEstimator(T& object, int n, double max_prime, int ksize, std::string input_file_name, char save_kmers, std::list<long> hash_list, bool rev_comp)
+CountEstimator<T>::CountEstimator(T& object, std::list<long> hash_list, int n, long max_prime, int ksize, std::string input_file_name, char save_kmers, bool rev_comp)
 {
+	if(n == 0)
+	{
+		cout<<"n=0 exception\n";
+		exit(0);
+	}
+	if(ksize == 0)
+	{
+		cout<<"ksize=0 exception\n";
+		exit(0);
+	}
+	if((ksize % 2) == 0)
+	{
+		cout<<"Due to an issue with khmer, only odd ksizes are allowed\n";
+		exit(0);
+	}
+	this->_ksize = ksize;
+	this->_hash_list = hash_list;
+	// get a prime to use for hashing
+	long p = get_prime_lt_x(max_prime);
+	this->p = p;
+	// initialize sketch to size n
+	
+
 
 }
 
@@ -72,6 +95,56 @@ template <typename T>
 void CountEstimator<T>::jaccard_vector()
 {
 
+}
+
+bool is_prime(int number)
+{
+	if(number < 2)
+	{
+		return false;
+	}
+	if(number == 2)
+	{
+		return true;
+	}
+	if((number % 2) == 0)
+	{
+		return false;
+	}
+	for(int i =3; i<(int(pow(number,0.5))+1); i+=2)
+	{
+		if((number%i) == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+long get_prime_lt_x(long target)
+{
+	if(target == 1)
+	{
+		return 1;
+	}
+	int i = int(target);
+	if( (i%2) == 0 )
+	{
+		i -= 1;
+	}
+	while(i > 0)
+	{
+		if(is_prime(i))
+		{
+			return i;
+		}
+		i -=2;
+	}
+	if(i <= 0)
+	{
+		cout<<"unable to find a prime number < "<< target <<endl;
+		exit(0);
+	}
 }
 
 int main()
--- minhash.hpp
+++ minhash.hpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <string>
 #include <list>
+#include <cmath>
 
 template <typename T >
 class CountEstimator
 {
+	/* A simple bottom n-sketch MinHash implementation.
+    n is the number of sketches to keep
+    Still don't know what max_prime is */
 private:
 	int n;
-	double _max_prime = 9999999999971.;
+	long _max_prime = 9999999999971;
 	int _ksize = 0;
 	std::string _input_file_name = NULL;
 	char _save_kmers = 'n';
@@ -15,7 +19,7 @@ private:
 	bool _rev_comp=false;
 
 public:
-	CountEstimator(T& object, int n, double max_prime, int ksize, std::string input_file_name, char save_kmers, std::list<long> hash_list, bool rev_comp);
+	CountEstimator(T& object, std::list<long> hash_list, int n = 0, long max_prime = 9999999999971, int ksize = 0, std::string input_file_name = NULL, char save_kmers = 'n', bool rev_comp = false);
 	void parse_file();
 	void down_sample();
 	void add();
@@ -28,6 +32,8 @@ public:
 	void _export();
 	void count_vector();
 	void jaccard_vector();
+	bool is_prime(int number);
+	long get_prime_lt_x(long target);
 
 };
 
